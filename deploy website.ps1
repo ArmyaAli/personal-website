4c4742359d02355a5ff80202604b25b2269791a9
armyaali <ali.umar.work@gmail.com>
2020-09-07 11:06:06 -0400

    initial commit, uploading my website code to github

---
+++ deploy website.ps1
@@ -0,0 +1,16 @@
+﻿Set-Location $PWD.Path
+
+$sources = "./assets",
+           "./index.html",
+           "./scripts", 
+           "./style", 
+           "./resume.pdf",
+           "./PDF.js"
+           "./particles.js"
+$dest = "./public"
+
+foreach ($source in $sources) {
+  Copy-Item $source $dest -Recurse -Force
+}
+
+firebase deploy
